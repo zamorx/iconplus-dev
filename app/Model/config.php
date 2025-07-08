89ac37a90278820bb1f7a4bb016531517f88d947
Carlos Zamora Morales <zamorx@gmail.com>
2025-07-07 23:48:16 -0400

    Update config.php

--- app/Model/config.php
+++ app/Model/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'iconplus.net');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'iconplus_user');
 define('DB_PASSWORD', 'AdminTA!2020');
 define('DB_NAME', 'iconplus_supportdb');
